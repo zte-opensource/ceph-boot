de21a2fe6cd6cf4132d7454a1e4086aa10738913
runsisi <runsisi@hust.edu.cn>
2018-04-10 17:05:39 +0800

    merge remote_execution_node into remote_execution
    Signed-off-by: runsisi <runsisi@hust.edu.cn>

--- remote_execution.go
+++ remote_execution.go
@@ -5,8 +5,22 @@ import (
 	"io"
 	"reflect"
 
-	"github.com/reconquest/hierr-go"
+    "golang.org/x/crypto/ssh"
+
+    "github.com/reconquest/hierr-go"
+    "github.com/zte-opensource/runcmd"
 )
+
+type remoteExecutionNode struct {
+    node    *distributedLockNode
+    command runcmd.CmdWorker
+
+    stdin  io.WriteCloser
+    stdout io.WriteCloser
+    stderr io.WriteCloser
+
+    exitCode int
+}
 
 type remoteExecution struct {
 	stdin io.WriteCloser
@@ -17,6 +31,54 @@ type remoteExecutionResult struct {
 	node *remoteExecutionNode
 
 	err error
+}
+
+func (node *remoteExecutionNode) wait() error {
+    err := node.command.Wait()
+    if err != nil {
+        _ = node.stdout.Close()
+        _ = node.stderr.Close()
+        if sshErrors, ok := err.(*ssh.ExitError); ok {
+            node.exitCode = sshErrors.Waitmsg.ExitStatus()
+
+            return fmt.Errorf(
+                `%s had failed to evaluate command, `+
+                    `remote command exited with non-zero code: %d`,
+                node.node.String(),
+                node.exitCode,
+            )
+        }
+
+        return hierr.Errorf(
+            err,
+            `%s failed to finish execution, unexpected error`,
+            node.node.String(),
+        )
+    }
+
+    err = node.stdout.Close()
+    if err != nil {
+        return hierr.Errorf(
+            err,
+            `%s can't close stdout`,
+            node.node.String(),
+        )
+    }
+
+    err = node.stderr.Close()
+    if err != nil {
+        return hierr.Errorf(
+            err,
+            `%s can't close stderr`,
+            node.node.String(),
+        )
+    }
+
+    return nil
+}
+
+func (node *remoteExecutionNode) String() string {
+    return node.node.String()
 }
 
 func (execution *remoteExecution) wait() error {
